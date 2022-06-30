afc76ee19e8dc5dd123c6bc9a531476d959cb5e0
juwwy <juwonolowu52@gmail.com>
2022-06-30 16:42:21 +0100

    first project commit

--- Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
+++ Project/03_coffee_shop_full_stack/starter_code/backend/src/api.py
@@ -1,10 +1,11 @@
+
 import os
 from flask import Flask, request, jsonify, abort
 from sqlalchemy import exc
 import json
 from flask_cors import CORS
 
-from .database.models import db_drop_and_create_all, setup_db, Drink
+from .database.models import db_drop_and_create_all, setup_db, Drink, db
 from .auth.auth import AuthError, requires_auth
 
 app = Flask(__name__)
@@ -17,9 +18,11 @@ CORS(app)
 !! NOTE THIS MUST BE UNCOMMENTED ON FIRST RUN
 !! Running this funciton will add one
 '''
-# db_drop_and_create_all()
+db_drop_and_create_all()
 
 # ROUTES
+
+#============ Drink Get all with short info session =================
 '''
 @TODO implement endpoint
     GET /drinks
@@ -29,7 +32,24 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/api/v1/drinks', methods=['GET'])
+@requires_auth('get:drinks')
+def get_all_drinks(b):
+    
+    try:
+        drinks = Drink.query.all()
+        print(drinks)
+        drink_response = [ drink.short() for drink in drinks ]
+       
+        return jsonify({
+            "success": True,
+            "drinks": drink_response
+        })
+    except Exception as error:
+        abort(422)
 
+
+#============ Drink Get By Detail session =================
 '''
 @TODO implement endpoint
     GET /drinks-detail
@@ -39,7 +59,23 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/api/v1/drinks-detail', methods=['GET'])
+@requires_auth('get:drinks-detail')
+def get_drinks_detail(b):
+    
+    try:
+        drinks = Drink.query.all()
+        drink_detail = [drink.long() for drink in drinks]
+        return jsonify({
+            "success": True,
+            "drinks": drink_detail
+        })
+    except Exception as error:
+        abort(422)
 
+
+
+#============ Drink Create session =================
 '''
 @TODO implement endpoint
     POST /drinks
@@ -51,6 +87,50 @@ CORS(app)
 '''
 
 
+try:
+    @app.route('/api/v1/drinks', methods=['POST'])
+    @requires_auth('post:drinks')
+    def create_drink(b):
+    
+
+        try:
+            recipe_data = []
+            request_data = request.get_json()
+            title = request_data['title']
+            recipes = request_data['recipe']
+            recipe ={}
+            for r in recipes:
+                recipe = {
+                    'name': r['name'],
+                    'color': r['color'],
+                    'parts': r['parts']
+                }
+                recipe_data.append(recipe)
+
+            result = json.dumps(recipe_data)
+            drink = Drink(title=title, recipe=result)
+            #print(drink)
+            Drink.insert(drink)
+            return jsonify({
+                "code": 201,
+                "success": True,
+                "drinks": drink,
+                "message": "Drink created successfully"
+            })
+
+        except:
+            db.session.rollback()
+            raise AuthError({
+                        "code": "unauthorize",
+                        'description': 'permission not inclusive for authentication token.'
+                    },401)
+            
+        finally:
+            db.session.close()
+except:
+    abort(403)
+
+#============ Drink Update session =================
 '''
 @TODO implement endpoint
     PATCH /drinks/<id>
@@ -62,8 +142,32 @@ CORS(app)
     returns status code 200 and json {"success": True, "drinks": drink} where drink an array containing only the updated drink
         or appropriate status code indicating reason for failure
 '''
+@app.route('/api/v1/drinks/<int:drink_id>', methods=['PATCH'])
+@requires_auth('patch:drinks')
+def patch_drink(b, drink_id):
+    
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).one_or_none()
+        request_data = request.get_json()
+        
+        drink.title = request_data['title']
+        rec = request_data['recipe']
+        drink.recipe = json.dumps(rec)
+        
+        Drink.update(drink)
+        return jsonify({
+            "code": 200,
+            "success": True,
+            "drinks": drink,
+            "message": "Drink updated successfully"
+        })
+    except:
+        db.session.rollback()
+        abort(401)
+    finally:
+        db.session.close()
 
-
+#============ Drink Delete session =================
 '''
 @TODO implement endpoint
     DELETE /drinks/<id>
@@ -75,6 +179,31 @@ CORS(app)
         or appropriate status code indicating reason for failure
 '''
 
+@app.route('/api/v1/drinks/<int:drink_id>', methods=['DELETE'])
+@requires_auth('delete:drinks')
+def delete_drink(b, drink_id):
+
+    try:
+        drink = Drink.query.filter(Drink.id == drink_id).first()
+        Drink.delete(drink)
+        return jsonify({
+            "code": 200,
+            "success": True,
+            "delete": drink_id,
+            "message": "Drink was deleted successfully!"
+        })
+    except:
+        db.session.rollback()
+        abort(404)
+    finally: 
+        db.session.close()
+
+
+
+
+
+
+#============ Drink Error Handling session =================
 
 # Error Handling
 '''
@@ -106,9 +235,31 @@ def unprocessable(error):
 @TODO implement error handler for 404
     error handler should conform to general task above
 '''
-
+@app.errorhandler(404)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 404,
+        "message": "resource not found"
+    }), 404
 
 '''
 @TODO implement error handler for AuthError
     error handler should conform to general task above
 '''
+
+@app.errorhandler(401)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": AuthError(error=error, status_code=401),
+        "message": "Not Authorize"
+    }), 401
+
+@app.errorhandler(403)
+def not_found(error):
+    return jsonify({
+        "success": False,
+        "error": 403,
+        "message": "Forbidden request. You don't have access to perform this action"
+    }), 403
